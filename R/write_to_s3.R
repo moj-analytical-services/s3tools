b43547f31458f68b755a0b8e7c70dba29511faa3
calumabarnett <calumabarnett@gmail.com>
2019-01-24 17:15:13 +0000

    add default sep argument, multipart argument and documentation

--- R/write_to_s3.R
+++ R/write_to_s3.R
@@ -69,12 +69,24 @@ write_file_to_s3 <- function(local_file_path, s3_path, overwrite=FALSE) {
 }
 
 
-# Same as write_df_to_csv_in_s3 but uses the R function write.table to enable arguments to be passed excluding header row, etc.
-
-write_df_to_table_in_s3 <- function(df, s3_path, overwrite=FALSE, ...) {
+#' Write an in-memory data frame to a csv file stored in S3
+#'
+#' @description This function is similar to \code{\link{write_df_to_csv_in_s3}} but uses \code{\link[utils]{write.table}} to write the data frame to a csv as opposed to \code{\link[utils]{write.csv}}. This allows the following additional arguments to be passed to \code{\link[utils]{write.table}}: \code{append}, \code{col.names}, \code{sep}, \code{dec}, \code{qmethod}.
+#'
+#' @param df A data frame you want to upload to S3.
+#' @param s3_path A character string containing the full path to where the file should be stored in S3, including any directory names and the bucket name.
+#' @param overwrite A logical indicating whether to overwrite the file if it already exists.
+#' @param multipart A logical indicating whether to use multipart uploads. See \url{http://docs.aws.amazon.com/AmazonS3/latest/dev/mpuoverview.html}. If \code{df} is less than 100 MB when written to csv, this is ignored.
+#' @param sep A string used to separate values within each row of \code{df}.
+#' @param ... Additional arguments passed to \code{write.table}.
+#' @return Returns nothing
+#' @export
+#'
+#' @examples write_df_to_table_in_s3(df, "alpha-everyone/delete/my_csv.csv")
+write_df_to_table_in_s3 <- function(df, s3_path, overwrite=FALSE, multipart=TRUE, sep=",", ...) {
   # write to an in-memory raw connection
   rc <- rawConnection(raw(0), "r+")
-  write.table(df, rc, ...)
+  write.table(df, rc, sep=sep, ...)
 
   # upload the object to S3
   credentials <- suppressMessages(get_credentials())
@@ -89,6 +101,7 @@ write_df_to_table_in_s3 <- function(df, s3_path, overwrite=FALSE, ...) {
                        bucket = p$bucket,
                        object = p$object,
                        check_region = TRUE,
+                       multipart = TRUE,
                        headers = c('x-amz-server-side-encryption' = 'AES256')))
   } else {
     close(rc)
